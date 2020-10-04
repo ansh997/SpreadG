621461be0243b5e08957c5dbb5dc02aac71ce57f
Himanshu Pal <22083123+ansh997@users.noreply.github.com>
2020-10-04 18:54:57 +0530

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="spreadG", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Himanshu Pal",
     author_email="palhimanshu997@gmail.com",
     description="Plot data from Google sheets",
